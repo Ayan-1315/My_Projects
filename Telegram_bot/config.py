fb7c1cc5fd063d9d9d5fe394d3731e3e011a4757
Ayan Sen <ayansen1315@gmail.com>
2024-11-17 23:03:00 +0530

    Update config.py

--- Telegram_bot/config.py
+++ Telegram_bot/config.py
@@ -1,3 +1,3 @@
-TOKEN = '7709076843:AAGMmTBEItlPgGhWPxJecJCjrpAbt8qN9ME'
-MONGO_DB_URI = "mongodb+srv://ayansen1315:9091157404@ayan1315.rbs2g.mongodb.net/?retryWrites=true&w=majority&appName=ayan1315"
-HF_TOKEN ='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'
+TOKEN = 'enter_your_telegram_api'
+MONGO_DB_URI = "enter_mongodb_uri"
+HF_TOKEN ='enter_huggingface_token' (optional)
